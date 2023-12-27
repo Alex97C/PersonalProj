e7b8915aeb15400f533f1618c885678ee0648b85
Alex97C <alexandru.clau97@gmail.com>
2023-12-27 20:05:43 +0200

    changes

--- project.cpp
+++ project.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
+class Animal{
+ private:
+    virtual int RunningSpeed() = 0;
+};
+
+class Dog : public Animal
+{
+public:
+    int RunningSpeed()
+    {
+        speed = 5;
+        return speed;
+    }
+
+
+
+private:
+    int speed;
+};
 int main()
 {
-    std::cout<<"I am here";
-
-    std::cout<<"Memme again";
+    Dog dogo;
+    std::cout<<dogo.RunningSpeed();
     return 0;
 }
